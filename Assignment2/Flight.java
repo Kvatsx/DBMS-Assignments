078566734eca798fd0f1b425c77b8376e0f22b55
Yashit Maheshwary <yashitmaheshwary@gmail.com>
2018-02-08 01:37:30 +0530

    Added Assignment2

---
+++ Assignment2/Flight.java
@@ -0,0 +1,14 @@
+/* ******************************************
+*  Author : Yashit   
+*  Created On : Thu Feb 08 2018
+*  File : Flight.java
+******************************************* */
+
+import java.io.*;
+import java.util.*;
+
+public class Flight {
+	public static void main(String[] args) throws IOException{
+		
+	}
+}
